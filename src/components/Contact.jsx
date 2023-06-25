0389b5a90aa0045d4aa8bf1b91e6e7f51e04e9fd
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-06-25 19:24:06 +0330

    Contact section is modified

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { FormContainer } from '../containers';
+import React from "react";
+import contact from "./../assets/Contact.jpg";
+import { FormContainer } from "../containers";
 import { motion } from "framer-motion";
 
 const imageVariant = {
@@ -22,27 +23,24 @@ const imageVariant = {
 
 const Contact = () => {
   return (
-    <div id='contact' className='bg-oddBgColor py-[40px] p-3'>
+    <div id="contact" className="bg-oddBgColor py-[40px] p-3">
       <p className="font-bold text-[30px] text-lightRoseBrown pb-3 text-center">
         Contact Me
       </p>
-      <div className='flex flex-wrap justify-center gap-2 w-full p-3 py-[40px]'>
-        <div className='w-[500px] h-[450px] object-cover bg-slate-600'>
-          picture
-          {/* <motion.img
-            variants={imageVariant}
-            initial="hidden"
-            whileInView="show"
-            src={aboutImage}
-            className="w-[200px] h-[300px] object-cover"
-          /> */}
-        </div>
-        <div className='p-2 text-justify w-[500px]'>
+      <div className="flex flex-wrap justify-center gap-2 w-full p-3 py-[40px]">
+        <motion.img
+          variants={imageVariant}
+          initial="hidden"
+          whileInView="show"
+          src={contact}
+          className="w-[500px] h-[450px] object-cover"
+        />
+        <div className="p-2 text-justify w-[500px]">
           <FormContainer />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
