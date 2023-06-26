21dd5bd4de6c654daea83ca8866b9fe4611cb363
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-06-26 22:28:00 +0330

    video placement is modified

--- src/containers/Videos.jsx
+++ src/containers/Videos.jsx
@@ -3,7 +3,7 @@ import { videos } from "./../assets/Contents";
 
 const Videos = () => {
   return (
-    <div className="py-[40px] p-3 flex justify-center">
+    <div className="py-[40px] p-3 flex gap-2 flex-wrap justify-center">
       {videos.map((item) => (
         <iframe
           key={item.id}
