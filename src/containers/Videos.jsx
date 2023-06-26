eec71427795635dcc712b639ed243027e286b9b9
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-06-26 22:15:40 +0330

    video container is added to collection

--- src/containers/Videos.jsx
+++ src/containers/Videos.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Videos = () => {
   return (
-    <div>Videos</div>
-  )
-}
+    <div className="py-[40px] p-3 flex justify-center">
+      <iframe
+        src="https://drive.google.com/file/d/1N7vbvF8Ere0x6jYj3irfXXHHWumlxM4y/preview"
+        className="w-[750px] h-[450px]"
+      ></iframe>
+    </div>
+  );
+};
 
-export default Videos
+export default Videos;
