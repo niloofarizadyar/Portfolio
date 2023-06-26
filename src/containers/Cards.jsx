7a1a220d59b86ae4bbf7238f26229fd729197223
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-06-26 23:51:23 +0330

    card container is added

--- src/containers/Cards.jsx
+++ src/containers/Cards.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { cards } from '../assets/Contents'
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className='py-[40px] p-3 flex gap-2 flex-wrap justify-center'>
+      {cards.map(item => <img key={item.id} className='w-[300px] h-[180px] object-cover' src={item.src} />)}
+      
+    </div>
   )
 }
 
