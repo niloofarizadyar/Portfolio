07e14bad1e575abff30bdb173164b52e00d56beb
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-06-29 19:42:04 +0330

    catalog container is added

--- src/containers/Catalogs.jsx
+++ src/containers/Catalogs.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Slider } from './../containers'
+import { catalogCharity } from "../assets/Contents";
+import { catalogSorena } from "../assets/Contents";
 
 const Catalogs = () => {
+  
   return (
-    <div>Catalogs</div>
-  )
-}
+    <div className="p-3 py-[40px] gap-2 flex flex-wrap justify-center items-center">
+      <Slider catalog={catalogCharity} />
+      <Slider catalog={catalogSorena} />
+    </div>
+  );
+};
 
-export default Catalogs
+export default Catalogs;
