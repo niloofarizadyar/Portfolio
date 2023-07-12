55ba33ce4c06442d87099a82ca3d475d34ac0d24
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-07-12 18:32:19 +0330

    catalog is modified

--- src/containers/Catalogs.jsx
+++ src/containers/Catalogs.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Slider } from './../containers'
+import { Slider } from "./../containers";
 import { catalogCharity } from "../assets/Contents";
 import { catalogSorena } from "../assets/Contents";
 
 const Catalogs = () => {
-  
   return (
     <div className="p-3 py-[40px] gap-2 flex flex-wrap justify-center items-center">
       <Slider catalog={catalogCharity} />
