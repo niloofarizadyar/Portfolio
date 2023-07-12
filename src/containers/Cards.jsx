c3aa4500926b73e2f401b2bb5dc99a40e7c90578
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-07-12 18:31:12 +0330

    card is modified

--- src/containers/Cards.jsx
+++ src/containers/Cards.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { cards } from '../assets/Contents'
+import React from "react";
+import { cards } from "../assets/Contents";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Cards = () => {
   return (
-    <div className='py-[40px] p-3 flex gap-2 flex-wrap justify-center'>
-      {cards.map(item => <img key={item.id} className='w-[300px] h-[180px] object-cover' src={item.src} />)}
-      
+    <div className="py-[40px] p-3 flex gap-2 flex-wrap justify-center">
+      {cards.map((item) => (
+        <LazyLoadImage
+          key={item.id}
+          className="w-[300px] h-[180px] object-cover"
+          src={item.src}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
